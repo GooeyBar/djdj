a957d32e30936225ab0026b6479aad7743c6d233
Daniel Tsioni <dtsioni@gmail.com>
2016-04-16 20:43:28 -0400

    Ch-ch-ch-changes

--- app.rb
+++ app.rb
@@ -13,21 +13,23 @@ end
 
 get '/:playlist_id' do
   playlist = Playlist.find_by(url_id: params[:playlist_id])
-  "#{playlist.name} : #{playlist.owner} : #{playlist.url_id}"
+  "#{playlist.name} : #{playlist.owner} : #{playlist.url_id} : #{playlist.users}"
 end
 
 post '/new' do
   owner = params[:owner]
   name = params[:name]
+  users = JSON.parse(request.body.read)
   url_id = SecureRandom.hex(3)
 
-  playlist = Playlist.new(owner: owner, name: name, url_id: url_id)
-
+  playlist = Playlist.new(owner: owner, name: name, url_id: url_id, users: users)
+  counter = 10
   if playlist.save
     redirect "/#{url_id}"
   else
-    until playlist.save
+    until playlist.save || counter <= 0
       playlist.url_id = SecureRandom.hex(3)
+      counter--
     end
     redirect "/#{url_id}"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416230337) do
+ActiveRecord::Schema.define(version: 20160417002723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160416230337) do
     t.string   "url_id"
     t.string   "name"
     t.string   "owner"
+    t.text     "users"
   end
 
 end
